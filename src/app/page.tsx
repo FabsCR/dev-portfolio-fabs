edc700d32d65c283215031c86bff3616ab6cbadd
FabsCR <fabfernandez@estudiantec.cr>
2024-11-12 04:55:29 -0600

    feat: add Certifications section with categorized list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { About } from "../components/About";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 import WhatsappButton from "../components/WhatsappButton";
+import { Certifications } from "../components/Certifications";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col gap-6">
       <About />
       <Projects />
+      <Certifications />
       <Contact />
       <WhatsappButton />
     </main>
