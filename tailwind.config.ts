e61d0cce5465f06fadd3f242746feb0ec7f2f57a
FabsCR <fabfernandez@estudiantec.cr>
2024-11-12 02:09:39 -0600

    feat: configure Tailwind CSS for dark mode, implement color scheme change (white to black), and integrate global styles and layout components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@headlessui/react/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#f7fafc", // color claro para el texto en modo oscuro
+        accent: "#4a5568",
+        background: "#1a202c", // fondo oscuro para modo oscuro
+        darkBox: "#2d3748", // fondo para cajas en modo oscuro
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
