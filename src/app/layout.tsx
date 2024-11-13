6e259bb8c18b240fd7b7fb4b006b291bc2beee59
FabsCR <fabfernandez@estudiantec.cr>
2024-11-13 03:35:31 -0600

    chore: update metadata and favicon configuration, remove unused .svg files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,21 @@ import "../app/globals.css";
 import { Theme } from "@radix-ui/themes";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Fabian Portfolio",
+  description: "Portfolio of Fabian Fernandez, showcasing projects, certifications, and contact information.",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+};
 
 export default function RootLayout({
   children,
